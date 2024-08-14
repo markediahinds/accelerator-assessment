6b21914ff0278f23ea1d13174e1eddb5498bef2e
Markedia Hinds <mhinds@pursuit.org>
2024-08-14 12:43:51 -0400

    Based off memory, 3 out of 5 routes are effective. Now I am going to refer to my notes to address bugs in the create and update routes. Encountering issues when attempt to use try/catch and if/else in one route.

--- backend/controllers/animesController.js
+++ backend/controllers/animesController.js
@@ -23,6 +23,26 @@ const {
 //       "description": "Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto. It tells the story of Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village."
 //   }
 // ]
+animes.get('/', async (req, res) => {
+  try {
+    const allAnimes = await getAllAnimes()
+    if(allAnimes[0]) {
+      res.status(200).json(allAnimes)
+    }
+  } catch (err) {
+    res.status(404).json({ error: err, message: `No Animes Found` })
+  }
+})
+
+animes.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const oneAnime = await getOneAnime(id)
+    if(oneAnime) {
+      res.status(200).json(oneAnime)
+    } else {
+    res.status(404).json({ error: `Anime Not Found` })
+    }
+})
 
 //Write a POST route that takes user provided data from the request body and creates a new anime in the database. The route should respond with a 201 status code and the new anime.
 //if the request body does not contain a name and description, or if the body's name or description have no length, respond with an error
@@ -33,6 +53,19 @@ const {
 //   "description": "this is anime"
 // }
 
+animes.post('/', async (req, res) => {
+  try {
+    const { name, description } = req.body
+    const createAnime = await createOneAnime(name, description)
+    if(createAnime.name && createAnime.description || createAnime.name.length == 0 || createAnime.description.length == 0) {
+      res.status(200).json(oneAnime)
+    }
+    res.status(201).json(createAnime)
+  } catch (err) {
+    res.status(404).json({ error: err, message: `Anime Not Found` })
+  }
+})
+
 //Write a PUT route that takes user provided data from the request body and updates an existing anime in the database. The route should respond with a 200 and the updated anime. The route should be able to handle a non-existent anime id.
 //if the request body does not contain a name and description, or if the body's name or description have no length, respond with an error
 //your response body should look this:
@@ -42,6 +75,16 @@ const {
 //   "description": "this is anime as well"
 // }
 
+animes.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const updateAnime = await updateOneAnime(id, ...req.body)
+    res.status(200).json(updateAnime)
+  } catch (err) {
+    res.status(404).json({ error: err, message: `Anime Not Found` })
+  }
+})
+
 //Write a DELETE route that deletes a single anime by id (provided by the client as a request param) from the database and responds with a 200 and the deleted anime data. The route should be able to handle a non-existent anime id.
 //your response body should look this:
 // {
@@ -49,4 +92,16 @@ const {
 //   "name": "test1",
 //   "description": "this is anime as well"
 // }
+
+animes.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const deleteAnime = await deleteOneAnime(id)
+    res.status(200).json(deleteAnime)
+  } catch (err) {
+    res.status(404).json({ error: err, message: `Anime Not Found`  })
+  }
+})
+
+
 module.exports = animes;
